d01b473d8fd057ed2a66a0f49ae3891965078efa
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-18 19:16:25 +0200

    lesson 2

---
+++ src/test/java/com/foretruff/junit/service/UserServiceTest.java
@@ -0,0 +1,14 @@
+package com.foretruff.junit.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserServiceTest {
+
+    @Test
+    void test() {
+        assertTrue(false);
+    }
+
+}
